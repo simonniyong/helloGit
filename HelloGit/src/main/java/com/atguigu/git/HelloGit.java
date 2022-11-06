88245944af6501d93137729da8550fafdfd4e57d
niyong <CCsimon88@@>
2022-11-06 16:00:17 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author yongni
+ * @date 2022/11/6 - 15:48
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+    }
+}
